2f11cd58ffdc161333c3fb6cdcd79ac2d453a627
sheepcao <sheepcao1986@163.com>
2014-11-15 23:17:03 +0800

    fix ios 8 bug.. actionsheet...v1.11

---
+++ DaysInLine/globalVars.h
@@ -0,0 +1,31 @@
+//
+//  globalVars.h
+//  DaysInLine
+//
+//  Created by 张力 on 13-11-5.
+//  Copyright (c) 2013年 张力. All rights reserved.
+//
+
+#ifndef DaysInLine_globalVars_h
+#define DaysInLine_globalVars_h
+
+int workArea[96] ;
+int lifeArea[96] ;
+NSString *modifyDate;
+NSString *userTips;
+NSString *password;
+
+//BOOL hasTheDay;
+
+int modifying;
+int modifyEventId;
+
+BOOL soundSwitch;
+BOOL remindSwitch;
+//int startDay; //1 = sunday, 2 = monday
+
+#define NR_IMAGEVIEW 5
+#define IMAGEVIEW_TAG_BASE 200
+//#define IMAGEBUTTON_TAG_BASE 300
+
+#endif
